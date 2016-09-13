6b3ca20b0808d982040fa7238c6e075f687e8756
Paul Keutelian <paulkeutelian@gmail.com>
2016-09-13 06:15:15 -0700

    #11 Added code for testing unsafe typing.

--- 3_FirstLast/39Type_Conversions.cpp
+++ 3_FirstLast/39Type_Conversions.cpp
@@ -3,8 +3,16 @@
 
 int Type_Conversions()
 {
-
+    double d = 0;
+    while (cin>>d) {        // repeat the statements 
+                            // as long as we type in numbers
+        int i = d;          // try to squeeze a double into an int
+        char c = i;         // try to squeeze and int into a char
+        int i2 = c;         // get the integer value of the character
+        cout << "d==" << d                  // the original double
+              << " i==" << i                // converted to int
+              << "i2 ==" << i2              // int value of char
+              << " char(" << c << ")\n";	// the char
+    }
     return 0;
 }
-
-// I changed this
