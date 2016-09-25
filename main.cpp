9bd8b19e37f074d56ca52161657b71bdd16456cb
Paul Keutelian <paulkeutelian@gmail.com>
2016-09-25 08:30:07 -0700

    Built in a functioning recycle menu.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 
 int helloworld();
 int Type_Conversions();
@@ -7,34 +8,45 @@ int String_Compare();
 int letter_writing();
 
 int main() {
-
-    std::cout << "Choose test program: \n"
-      << "    0) Exit Program.\n"
-      << "<< Chapter 2 >>\n"
-      << "    1) Hello World\n"
-      << "<< Chapter 3 >>\n"
-      << "    2) Type Conversions\n"
-      << "    3) First-Last \n"
-      << "    4) String_Compare\n"
-      << "    5) Exercise: Letter Writing\n"
-      << "Choice then ENTER: > ";
+    int on = 1;
     int choice = -1;
-    std::cin >> choice;
 
-    switch(choice) {
-        case 0 : break;
-        case 1 : helloworld();
-                 break;
-        case 2 : Type_Conversions();
-                 break;
-        case 3 : FirstLast();
-                 break;
-        case 4 : String_Compare();
-                 break;
-        case 5 : letter_writing();
-                 break;
-        default: std::cout << "Not a valid choice\n";
+    while (on==1) {
 
+        std::cout << "\n\n\nChoose test program: \n"
+          << "    0) Exit Program.\n"
+          << "<< Chapter 2 >>\n"
+          << "    1) Hello World\n"
+          << "<< Chapter 3 >>\n"
+          << "    2) Type Conversions\n"
+          << "    3) First-Last \n"
+          << "    4) String_Compare\n"
+          << "    5) Exercise: Letter Writing\n"
+          << "\n\n"
+          << "Choice then ENTER: > ";
+
+        std::cin.clear();
+        std::cin >> choice;
+        std::cin.ignore(std::numeric_limits<int>::max(), '\n');
+
+        std::cout << "\n\n";
+
+        switch(choice) {
+            case 0 : on = 0;
+                     break;
+            case 1 : helloworld();
+                     break;
+            case 2 : Type_Conversions();
+                     break;
+            case 3 : FirstLast();
+                     break;
+            case 4 : String_Compare();
+                     break;
+            case 5 : letter_writing();
+                     break;
+            default: std::cout << choice << " is not a valid choice.\n";
+
+        }
     }
     return 0;
 }
