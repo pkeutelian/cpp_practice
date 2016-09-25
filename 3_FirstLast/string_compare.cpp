8eebb3f38fc734ced34624774eddbec895a94d76
Paul Keutelian <paulkeutelian@gmail.com>
2016-09-25 09:05:34 -0700

    #20 Added note to prompt informing user to use CTRL+D to return to main menu.

--- 3_FirstLast/string_compare.cpp
+++ 3_FirstLast/string_compare.cpp
@@ -2,12 +2,16 @@
 #include "std_lib_facilities.h"
 
 int String_Compare()
-{
+{  
     int number_of_words = 0;
     string previous = " ";		// previous word; initialized to "not a word"
-    string current;			// current word
+    string current;			    // current word
+    cout << "Enter a series of words to find repeated words. 'CTRL+D' at any " 
+         << "time to exit. \n\n > ";    // #20 fix. 
+    
     while (cin>>current) {		// read a stream of words
         ++number_of_words;		// increase word count
+
         if (previous == current)	// check if the word is the same as last
             cout << "word number " << number_of_words 
                  << " repeated: " << current << '\n';
@@ -15,5 +19,3 @@ int String_Compare()
     }
     return 0;
 }
-
-// I changed this
